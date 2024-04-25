553facca782d4953cf49b622559f48e81292c0c1
noblica06 <noblica06@gmail.com>
2024-04-25 13:54:20 +0200

    added the login for admin

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -2,10 +2,30 @@
 class Admin extends Controller{
     public function index(){
         if(isset($_SESSION["loggedIn"])){
-        $this->view('/admin/index');
+        $this->view('/admin/dashboard');
         }else{
             $this->view('/admin/login');
         }
     }
-
+    
+    public function login(){
+        $username = filter_input(INPUT_POST,'username',FILTER_SANITIZE_EMAIL);
+        $password = $_POST['password'];
+        $statement = $this->connection->prepare("SELECT * FROM user WHERE username = ?");
+        $statement->bind_param('s', $username);
+        $statement->execute();
+        $result = $statement->get_result();
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            if (password_verify($password, $user['Password'])) {
+                $_SESSION['loggedIn'] = true;
+                $this->view('admin/dashboard');
+            } else {
+                echo 'Incorrect Password';
+            }
+           
+        } else {
+            echo "User does not exist.";
+        }
+    }
 }
--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -1,11 +1,11 @@
 <?php
-$email = $password = '';
+$username = $password = '';
 ?>
 
 <form action="/php/public/admin/login" method="POST">
     <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" value="<?php echo $email; ?>">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" class="form-control" id="username" name="username" value="<?php echo $username; ?>">
     </div>
     <br>
     <div class="mb-3">
