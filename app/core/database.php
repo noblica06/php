db5f416c8dbc94c48c165b4b7451f648f451f06c
noblica06 <noblica06@gmail.com>
2024-04-25 20:29:34 +0200

    tweaked the database connection a bit

--- app/core/database.php
+++ app/core/database.php
@@ -5,6 +5,7 @@
     define('DB_NAME', 'php');
 
     function connectDB() {
+        
         $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
         
         // Check connection
@@ -13,32 +14,32 @@
         }
         
 
-        // $sql = "SHOW TABLES LIKE product";
-        // $result = $conn->query($sql);
-        // if ($result->num_rows == 0) {
-        //     $sql = "CREATE TABLE 'php'.'product' ('ID' INT NOT NULL AUTO_INCREMENT , 'Path' TEXT NOT NULL , 'Title' VARCHAR(255) NOT NULL ,'Description' VARCHAR(255) NOT NULL , PRIMARY KEY ('ID')) ENGINE = InnoDB;";
-        //     if(!$conn->query($sql)){
-        //         echo 'Table product could not be created!';
-        //     }
-        // }
+        $sql = "SELECT * FROM information_schema.tables WHERE table_name = 'product'";
+        $result = $conn->query($sql);
+        if ($result->num_rows == 0) {
+            $sql = "CREATE TABLE product ('ID' INT NOT NULL AUTO_INCREMENT , 'Path' TEXT NOT NULL , 'Title' VARCHAR(255) NOT NULL ,'Description' VARCHAR(255) NOT NULL , PRIMARY KEY ('ID'))";
+            if(!$conn->query($sql)){
+                echo 'Table product could not be created!';
+            }
+        }
 
-        // $sql = "SHOW TABLES LIKE user";
-        // $result = $conn->query($sql);
-        // if ($result->num_rows == 0) {
-        //     $sql = "CREATE TABLE 'php'.'user' ('ID' INT NOT NULL AUTO_INCREMENT , 'Username' TEXT NOT NULL , 'Password' VARCHAR(255) NOT NULL , PRIMARY KEY ('ID')) ENGINE = InnoDB;";
-        //     if(!$conn->query($sql)){
-        //         echo 'Table user could not be created!';
-        //     }
-        // }
+        $sql = "SELECT * FROM information_schema.tables WHERE table_name = 'user'";
+        $result = $conn->query($sql);
+        if ($result->num_rows == 0) {
+            $sql = "CREATE TABLE user ('ID' INT NOT NULL AUTO_INCREMENT , 'Username' TEXT NOT NULL , 'Password' VARCHAR(255) NOT NULL , PRIMARY KEY ('ID')) ";
+            if(!$conn->query($sql)){
+                echo 'Table user could not be created!';
+            }
+        }
 
-        // $sql = "SHOW TABLES LIKE comment";
-        // $result = $conn->query($sql);
-        // if ($result->num_rows == 0) {
-        //     $sql = "CREATE TABLE 'php'.'comment' ('ID' INT NOT NULL AUTO_INCREMENT , 'Name' VARCHAR(255) NOT NULL , 'Email' VARCHAR(255) NOT NULL, 'Body' TEXT NOT NULL , PRIMARY KEY ('ID')) ENGINE = InnoDB;";
-        //     if(!$conn->query($sql)){
-        //         echo 'Table user could not be created!';
-        //     }
-        // }
+        $sql = "SELECT * FROM information_schema.tables WHERE table_name = 'comment'";
+        $result = $conn->query($sql);
+        if ($result->num_rows == 0) {
+            $sql = "CREATE TABLE comment ('ID' INT NOT NULL AUTO_INCREMENT , 'Name' VARCHAR(255) NOT NULL , 'Email' VARCHAR(255) NOT NULL, 'Body' TEXT NOT NULL , PRIMARY KEY ('ID'))";
+            if(!$conn->query($sql)){
+                echo 'Table user could not be created!';
+            }
+        }
     
         return $conn;
     }
